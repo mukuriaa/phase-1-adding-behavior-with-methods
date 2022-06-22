2513dbcd5ab98b94d1cab447f5af3f44181f89e4
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-22 00:19:42 -0700

    commit tree 006e5a3b852a1b9895495a0b7f00dd15cbdc61ae

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Cat{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    get sound(){
+        return this.sound();
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+  
+}
+class Dog{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    get sound(){
+        return this.sound();
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+class Bird{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    get sound(){
+        return this.sound();
+    }
+    speak(){
+        if(this.sex==="male"){
+        return `It\'s me! ${this.name}, the parrot!`
+    }else if(this.sex ==="female"){
+        return `${this.name} says squawk!`
+
+    }
+    }
+}
+//const cat=new Cat("Mukuria");
